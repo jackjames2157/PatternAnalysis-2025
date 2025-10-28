5df0b9396bcf74c177c55aed02b0ea342978e81b
Jack James <j.james1@uq.net.au>
2025-10-29 09:25:52 +1000

    Update modules.py with Denormalisation
    Adding function for image denormalisation, including test values and a print to ensure that the function is working

--- recognition/project1unet-45802317/modules.py
+++ recognition/project1unet-45802317/modules.py
@@ -14,15 +14,39 @@ import torch.nn as nn
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 # Check if CUDA is available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 """
-Chose 21 for the random seed because it's my favourite number, no functional reason
+Chose 21 for the random seed because it's my favourite number, no functional
+reason
 """
 torch.manual_seed(21)
 np.random.seed(21)
 random.seed(21)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(21)
+
+"""
+The next step is to create the required visualisation functions. One key
+consideration here is that the 2D Oasis data is in grayscale rather than 
+colour, which changes the normalisation process. To start with, will
+use 0.5 as mean value and 0.25 as standard deviation value rather than the 
+imagenet values (which are for RGB).
+"""
+
+# Test tensor to ensure denormalisation is working (will be removed later)
+tensor = torch.Tensor([0, 1.5, 0.5, 1])
+
+# Denormalisation function
+def denormalisation(tensor):
+  mean = torch.tensor([0.5]).view(1,1,1)
+  std = torch.tensor([0.25]).view(1,1,1)
+  denormalise_tensor = tensor * std + mean
+  # Ensure values are between 0 and 1
+  return torch.clamp(denormalise_tensor, 0, 1)
+
+# Test output to ensure that denormalisation is working (will be removed later)
+print(denormalisation(tensor))
